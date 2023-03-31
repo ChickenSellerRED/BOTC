1f213820c3033856c82e62b562d4b3a980f4a22c
ChickenSellerRED <jizebi@usc.edu>
2023-03-31 02:28:53 -0700

    add websocket to connect server

--- lib/Pages/SwitchGameModePage.dart
+++ lib/Pages/SwitchGameModePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:web_socket_channel/io.dart';
 
 import '../common/Global.dart';
 
--- lib/Pages/WaitInRoomPage.dart
+++ lib/Pages/WaitInRoomPage.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_flutter_app/Models/Room.dart';
 import 'package:my_flutter_app/Widgets/UserCardWidget.dart';
+import 'package:web_socket_channel/io.dart';
 import '../common/Global.dart';
 import '../Models/User.dart';
+import 'dart:convert';
 
 class WaitInRoomPage extends StatefulWidget{
   final Room _room;
@@ -17,6 +21,26 @@ class WaitInRoomPage extends StatefulWidget{
 }
 
 class WaitInRoomPageState extends State<WaitInRoomPage>{
+   late final channel;
+   late List<User> users;
+  @override
+  void initState() {
+    channel = IOWebSocketChannel.connect('ws://10.0.2.2:3000');
+    users = <User>[];
+    channel.sink.add(jsonEncode({
+      "verb":"create_room",
+      "body":{
+        "maxPeople":998,
+        "name":"五影会谈",
+      }
+    }));
+  }
+
+
+   @override
+  void setState(VoidCallback fn) {
+
+  }
 
   void _exitRoom(){}
   Widget build(BuildContext context) {
@@ -39,14 +63,28 @@ class WaitInRoomPageState extends State<WaitInRoomPage>{
                 SizedBox(
                   width: 300,
                   height: 200,
-                  child: GridView.builder(
-                    scrollDirection: Axis.vertical,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-                    itemCount: Global.userLab.count(),
-                    itemBuilder: (context,index){
-                      return UserCardWidget(Global.userLab.getUser(index));
-                    },
-                  ),
+
+                  child: StreamBuilder(
+                    stream:channel.stream,
+                    builder:(context, snapshot){
+                      if(snapshot.hasData){
+                        String verb = jsonDecode(snapshot.data as String)["verb"];
+                        //todo:memberlist输出空值 待解决
+                        if(verb == "create_room_success")
+                          print(jsonDecode(snapshot.data as String)["memberList"]);
+                      }
+                      return GridView.builder(
+                          scrollDirection: Axis.vertical,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                          itemCount: this.users.length,
+                          itemBuilder: (context,index){
+                          return UserCardWidget(this.users[index]);
+                        },
+                      );
+                  }
+                    ,
+                  )
+
                 ),
                 Row(
                   children: <Widget>[
